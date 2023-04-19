45788bc05440a50b62246001a989fbd389eb519e
Sarah Kuss <sarah.kuss33@gmail.com>
2023-04-19 16:42:31 -0400

    added useState

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,19 +1,25 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 export default function About () {
-  
+  const [imgUrl, setImgUrl] = useState("https://picsum.photos/300/300")
+  const [nameColor, setNameColor] = useState("blue")
   return (
     <main>
-      <Container className="bg-danger">
+      <Container>
         <Row className="text-center">
           <Col sm={12} md={6}>
-            <img src="https://picsum.photos/300/300"
+            <img 
+            src={imgUrl}
             className="rounded-circle"
             alt="profile" />
+          <Button onClick={() => setImgUrl("https://picsum.photos/300/300?random")}>Change Image</Button>
+
+          <Button onClick={() => setNameColor("red")}>Change Color</Button>
           </Col>
           
           <Col>
-            <h1 className="mt-3">Sarah Kuss</h1>
+            <h1 style={{color: nameColor}} className="mt-3">Sarah Kuss</h1>
             <p>Former zookeeper, future Software Engineer,  student of life- continuously learning and growing. I left my dream career to pursue a better future; one that allows me the freedom to give back to my community and contribute to conservation efforts. I love animals, plants, traveling, surfing, and playing guitar. My goal after completing the course is to get a job as a junior software engineer where I can continue growing my skills. 
             </p>
           </Col>
